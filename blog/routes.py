e0e770ce6649b65f0fd918dfb274def7aa66d754
abdullahskartal <abdullahsaidkartal@gmail.com>
2018-07-27 15:07:28 +0300

    Created image fixer with Pillow

--- blog/routes.py
+++ blog/routes.py
@@ -1,5 +1,6 @@
 import os
 import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect,request
 from blog.models import User, Post
 from blog.forms import RegisterForm,LoginForm,UpdateAccount
@@ -24,7 +25,7 @@ posts = [
 @app.route("/")
 def index():
     return render_template("index.html",posts = posts)
-    
+
 # About
 @app.route("/about")
 def about():
@@ -73,7 +74,13 @@ def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _,f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path,"static/profile",picture_fn)
+    picture_path = os.path.join("static\\profile",picture_fn)
+    
+    output_size =(125,125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+
+    i.save(picture_path)
     return picture_fn
 
 # Account
@@ -87,7 +94,7 @@ def account():
             current_user.image_file = picture_file
         current_user.username = form.username.data
         current_user.email = form.email.data
-        db.session.commit()
+        db.session.commit() 
         flash("Your account has been updated.","success")
         return redirect(url_for("account"))
     elif request.method == "GET":
